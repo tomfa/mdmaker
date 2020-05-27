08ee57581380912346322d418df5368e316d774a
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-05-28 01:51:03 +0200

    parsers: add wordpress xml parser

---
+++ src/parsers/tests/wordpress-xml.test.js
@@ -0,0 +1,48 @@
+const parseWordpressXML = require("../wordpress-xml");
+const resolve = require("path").resolve;
+
+describe("Wordpress XML parser", () => {
+  const path = resolve(__dirname, "./export.xml");
+  let posts;
+  beforeAll(async () => {
+    posts = await parseWordpressXML(path);
+  });
+  test("ignores non-post item", () => {
+    expect(posts.length).toBe(1);
+  });
+
+  test("identifies wordpress title and slug", () => {
+    const { slug, title } = posts[0];
+
+    expect(slug).toBe("adding-freshdesk-to-slack");
+    expect(title).toBe("Adding Freshdesk to Slack");
+  });
+
+  test("identifies post status", () => {
+    const { status } = posts[0];
+
+    expect(status).toBe("publish");
+  });
+
+  test("returns a Date object for 'date'", () => {
+    const post = posts[0];
+    expect(post.date).toBeInstanceOf(Date);
+  });
+
+  test("finds img urls in content", () => {
+    const post = posts[0];
+    expect(post.filePaths).toEqual([
+      "http://notes.webutvikling.org/wp-content/uploads/2016/08/Screen-Shot-2016-08-02-at-15.27.24-300x98.png",
+    ]);
+  });
+
+  test("finds find tag", () => {
+    const post = posts[0];
+    expect(post.tags).toEqual(["freshdesk", "slack"]);
+  });
+
+  test("finds find categories", () => {
+    const post = posts[0];
+    expect(post.categories).toEqual(["Slack"]);
+  });
+});
---
+++ src/parsers/wordpress-xml.js
@@ -0,0 +1,80 @@
+const swear = require("../utils/promiser");
+const xml2js = require("xml2js");
+
+const readFile = require("../utils/file-handler").readFile;
+const ParseException = require("../utils/errors").ParseException;
+
+async function process(path) {
+  try {
+    const parser = new xml2js.Parser();
+    const data = await swear(readFile, path);
+    const result = await swear(parser.parseString, data);
+    const xmlData = result.rss.channel[0].item;
+    const xmlPosts = xmlData.filter(
+      (item) => item["wp:post_type"][0] === "post"
+    );
+    return xmlPosts.map(parseXMLPost);
+  } catch (err) {
+    throw new ParseException(`Unable to read ${path}: ${err}`);
+  }
+}
+
+function parseXMLPost(post) {
+  const author = post["dc:creator"][0];
+  const url = post.guid[0]["_"];
+  const title = post.title[0];
+  const publishedDate = new Date(post.pubDate);
+  const content = post["content:encoded"][0];
+  const status = post["wp:status"][0];
+  const slug = post["wp:post_name"][0];
+
+  const categories = parseCategories(post);
+  const tags = parseTags(post);
+  const filePaths = getFilePaths(content);
+
+  return {
+    author,
+    url,
+    title,
+    date: publishedDate,
+    data: content,
+    status,
+    slug,
+    categories,
+    tags,
+    filePaths,
+  };
+}
+
+function getFilePaths(content) {
+  /*
+   * Returns all url paths that can be considered to be downloaded and replaced
+   * */
+  const filePattern = new RegExp('(?:src="(.*?)")', "gi");
+  const matches = [];
+
+  let m;
+  while ((m = filePattern.exec(content)) !== null) {
+    matches.push(m[1]);
+  }
+
+  return matches;
+}
+
+function parseCategories(post) {
+  const xmlCategories = post.category || [];
+  return xmlCategories
+    .filter((c) => c.$.domain === "category")
+    .map((c) => c._)
+    .filter((c) => c !== "Uncategorized");
+}
+
+function parseTags(post) {
+  const xmlCategories = post.category || [];
+  return xmlCategories
+    .filter((c) => c.$.domain === "post_tag")
+    .map((c) => c._)
+    .filter((c) => c !== "Uncategorized");
+}
+
+module.exports = process;
