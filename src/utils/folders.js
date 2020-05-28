1310b452cebf33f307d575c91da4cb0f323ecead
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-05-28 20:15:34 +0200

    utils: add folders.createFolder

--- src/utils/folders.js
+++ src/utils/folders.js
@@ -1,9 +1,14 @@
+const fs = require("fs");
 const dateFormat = require("dateformat");
 
 function getFolderName(post, folderFormat) {
   return dateFormat(post.date, folderFormat.replace("slug", `"${post.slug}"`));
 }
+function createFolder(path) {
+  return fs.promises.mkdir(path, { recursive: true });
+}
 
 module.exports = {
   getFolderName,
+  createFolder,
 };
--- src/utils/tests/folders.test.js
+++ src/utils/tests/folders.test.js
@@ -1,4 +1,6 @@
-const { getFolderName } = require("../folders");
+const { getFolderName, createFolder } = require("../folders");
+const { resolve } = require("path");
+const fs = require("fs");
 
 const assert = (anything) => expect(anything).toBe(true);
 
@@ -27,5 +29,38 @@ describe("folder utils", () => {
     test("Replaces slug with slug of post", () => {
       assert(defaultFolderName.endsWith("-my-post-name"));
     });
+  });
+
+  describe("createFolder", () => {
+    const folderName = resolve(__dirname, `./tmp`);
+    const nestedFolderName = resolve(__dirname, `./tmp2/deep/nested`);
+
+    const cleanUp = async () => {
+      for (const folder of [folderName, nestedFolderName]) {
+        if (fs.existsSync(folder)) {
+          await fs.promises.rmdir(folder);
+        }
+      }
+    };
+
+    beforeAll(cleanUp); // In case of failed run
+
+    test("creates a folder", async () => {
+      expect(!fs.existsSync(folderName)).toBe(true);
+
+      await createFolder(folderName);
+
+      expect(fs.existsSync(folderName)).toBe(true);
+    });
+
+    test("creates parent folders", async () => {
+      expect(!fs.existsSync(nestedFolderName)).toBe(true);
+
+      await createFolder(nestedFolderName);
+
+      expect(fs.existsSync(nestedFolderName)).toBe(true);
+    });
+
+    afterAll(cleanUp);
   });
 });
