2b5fc700cec50e541752fdf2d8d0d766869e75d5
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-05-28 19:38:14 +0200

    utils: add makePromise method

--- src/utils/promiser.js
+++ src/utils/promiser.js
@@ -3,11 +3,16 @@
  *
  * Replaces callback functions with Promises
  *
- * Example (reading a file):
+ * Example – reading a file with fs:
  *   const fs = require('fs');
  *
  *   const path = '/path/to/file.txt'
  *   const fileContent = await swear(fs.readFile, path)
+ *
+ * Example – creating an async method of fs.readFile
+ *   const readFile = makePromise(fs.readFile);
+ *
+ *   const fileContent = await readFile(path);
  */
 
 async function swear(oldFunction, args) {
@@ -20,4 +25,18 @@ async function swear(oldFunction, args) {
   });
 }
 
-module.exports = swear;
+function makePromise(oldFunction) {
+  return (...args) => {
+    return new Promise((resolve, reject) => {
+      try {
+        oldFunction(...args, (err, data) =>
+          err ? reject(err) : resolve(data)
+        );
+      } catch (exception) {
+        reject(exception);
+      }
+    });
+  };
+}
+
+module.exports = { swear, makePromise };
