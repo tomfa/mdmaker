6a9000ea361bd1a81fa40af84f53d7b79e7c3672
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-05-30 21:25:40 +0200

    Extract cli to bin/mdmaker.js

---
+++ bin/mdmaker.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+const resolve = require("path").resolve;
+
+const run = require("../src/index");
+const urlUtils = require("../src/utils/urls");
+
+const defaultParser = "./parsers/wordpress-xml";
+const defaultTemplate = "./templates/gatsby.md";
+
+const argv = require("yargs")
+  .usage("yarn convert <input-file> [args]")
+  .example(
+    "yarn convert wordpress.xml",
+    "generates markdown files based on wordpress xml export"
+  )
+  .alias("d", "download-images")
+  .default("d", false)
+  .describe("d", "Downloads images refernces to poot folder.")
+  .alias("f", "folder-format")
+  .default("f", 'yyyy-mm-dd-"slug"')
+  .describe("f", "Format of individual post folder name.")
+  .alias("o", "output-dir")
+  .default("o", "output")
+  .describe("o", "Folder in which to put posts")
+  .alias("i", "filter-images")
+  .default("i", urlUtils.defaultRegex)
+  .describe(
+    "i",
+    "Regex filter for which linked images to download and replace urls."
+  )
+  .alias("p", "parser")
+  .default("p", defaultParser)
+  .describe("p", "Which parser to use for parsing input file.")
+  .alias("s", "filter-slug")
+  .describe("s", "Specify post slug if wish to convert a single post")
+  .alias("t", "template")
+  .default("t", defaultTemplate)
+  .describe("t", "Which template to use for generating files.")
+  .help("h")
+  .alias("h", "help").argv;
+
+const parser = require(argv.parser !== defaultParser
+  ? resolve(process.cwd(), argv.parser)
+  : resolve(__dirname, `../src/${argv.parser}`));
+
+const template =
+  argv.template !== defaultTemplate
+    ? resolve(process.cwd(), argv.template)
+    : resolve(__dirname, `../src/${argv.template}`);
+
+const { downloadImages, folderFormat, filterImages, outputDir, slug } = argv;
+const inputArg = (argv._.length && argv._[0]) || null;
+
+if (!inputArg) {
+  console.log("Missing input file. Run with --help for assistance.");
+  return;
+}
+
+const inputFile = resolve(process.cwd(), inputArg);
+
+run({
+  inputFile,
+  downloadImages,
+  folderFormat,
+  filterImages,
+  outputDir,
+  parser,
+  slugFilter: slug,
+  templatePath: template,
+});
--- src/index.js
+++ src/index.js
@@ -1,118 +1,93 @@
-const resolve = require("path").resolve;
-
 const { downloadFiles, readFile, writeFile } = require("./utils/file-handler");
 const urlUtils = require("./utils/urls");
 const { getFolderName, createFolder } = require("./utils/folders");
 const { htmlToMd } = require("./utils/markdown");
 const { insertVariables } = require("./utils/templates");
 
-const defaultParser = "./parsers/wordpress-xml";
-const defaultTemplate = "./templates/gatsby.md";
+async function convertPost({
+  post,
+  template,
+  templatePath,
+  outputDir,
+  folderFormat,
+  downloadImages,
+  filterImages,
+}) {
+  const baseUrl = urlUtils.findBaseUrl(post.url);
+  const postFolder = getFolderName(post, folderFormat);
+  const outputFolder = `${outputDir}/${postFolder}`;
 
-const argv = require("yargs")
-  .usage("yarn convert <input-file> [args]")
-  .example(
-    "yarn convert wordpress.xml",
-    "generates markdown files based on wordpress xml export"
-  )
-  .alias("d", "download-images")
-  .default("d", false)
-  .describe("d", "Downloads images refernces to poot folder.")
-  .alias("f", "folder-format")
-  .default("f", 'yyyy-mm-dd-"slug"')
-  .describe("f", "Format of individual post folder name.")
-  .alias("o", "output-dir")
-  .default("o", "output")
-  .describe("o", "Folder in which to put posts")
-  .alias("i", "filter-images")
-  .default("i", urlUtils.defaultRegex)
-  .describe(
-    "i",
-    "Regex filter for which linked images to download and replace urls."
-  )
-  .alias("p", "parser")
-  .default("p", defaultParser)
-  .describe("p", "Which parser to use for parsing input file.")
-  .alias("s", "filter-slug")
-  .describe("s", "Specify post slug if wish to convert a single post")
-  .alias("t", "template")
-  .default("t", defaultTemplate)
-  .describe("t", "Which template to use for generating files.")
-  .help("h")
-  .alias("h", "help").argv;
+  console.log(`Converting ${post.slug} -> ${outputDir}`);
+  await createFolder(outputFolder);
 
-const parser = require(argv.parser !== defaultParser
-  ? resolve(process.cwd(), argv.parser)
-  : resolve(__dirname, argv.parser));
+  let htmlContent = post.content;
 
-const template =
-  argv.template !== defaultTemplate
-    ? resolve(process.cwd(), argv.template)
-    : resolve(__dirname, argv.template);
-const { downloadImages, folderFormat, filterImages, outputDir, slug } = argv;
-const inputArg = (argv._.length && argv._[0]) || null;
+  if (downloadImages) {
+    // Find files to download
+    htmlContent = urlUtils.makeUrlsAbsolute({
+      content: post.content,
+      path: post.url,
+    });
 
-if (!inputArg) {
-  console.log("Missing input file. Run with --help for assistance.");
-  return;
+    const fileUrls = urlUtils.extractUrls({
+      content: htmlContent + (post.image ? ` <img src="${post.image}" />` : ""),
+      filterDomain: baseUrl,
+      regexp: filterImages,
+    });
+
+    // Download files
+    downloadFiles({ urls: fileUrls, to: outputFolder });
+
+    // Make downloaded files references as local files
+    post.image = urlUtils.makeUrlRelative({ url: post.image });
+    const urlMapping = fileUrls.map((url) => ({
+      external: url,
+      internal: urlUtils.makeUrlRelative({ url }),
+    }));
+    urlMapping.forEach(
+      (m) => (htmlContent = htmlContent.replace(m.external, m.internal))
+    );
+  }
+
+  // Write as markdown
+  const markdownContent = htmlToMd(htmlContent);
+  const content = insertVariables({
+    template,
+    variables: { ...post, content: markdownContent, html: htmlContent },
+  });
+  const templateExt = templatePath.split(".").reverse()[0];
+  await writeFile(`${outputFolder}/index.${templateExt}`, content);
 }
 
-const inputFile = resolve(process.cwd(), inputArg);
-
-async function run() {
+async function convert({
+  inputFile,
+  slugFilter,
+  templatePath,
+  parser,
+  outputDir,
+  folderFormat,
+  downloadImages,
+  filterImages,
+}) {
   const posts = (await parser(inputFile)).filter(
-    (p) => !slug || p.slug === slug
+    (p) => !slugFilter || p.slug === slugFilter
   );
-  const markdownTemplate = await readFile(template, "utf-8");
+  const template = await readFile(templatePath, "utf-8");
 
   console.log(`Converting ${posts.length} posts...`);
-  posts.forEach(async (post) => {
-    const baseUrl = urlUtils.findBaseUrl(post.url);
-    const postFolder = getFolderName(post, folderFormat);
-    const outputFolder = `${outputDir}/${postFolder}`;
-
-    console.log(`Converting ${post.slug} -> ${outputDir}`);
-    await createFolder(outputFolder);
-
-    let htmlContent = post.content;
-
-    if (downloadImages) {
-      // Find files to download
-      htmlContent = urlUtils.makeUrlsAbsolute({
-        content: post.content,
-        path: post.url,
-      });
-
-      const fileUrls = urlUtils.extractUrls({
-        content:
-          htmlContent + (post.image ? ` <img src="${post.image}" />` : ""),
-        filterDomain: baseUrl,
-        regexp: filterImages,
-      });
-
-      // Download files
-      downloadFiles({ urls: fileUrls, to: outputFolder });
-
-      // Make downloaded files references as local files
-      post.image = urlUtils.makeUrlRelative({ url: post.image });
-      const urlMapping = fileUrls.map((url) => ({
-        external: url,
-        internal: urlUtils.makeUrlRelative({ url }),
-      }));
-      urlMapping.forEach(
-        (m) => (htmlContent = htmlContent.replace(m.external, m.internal))
-      );
-    }
-
-    // Write as markdown
-    const markdownContent = htmlToMd(htmlContent);
-    const content = insertVariables({
-      template: markdownTemplate,
-      variables: { ...post, content: markdownContent, html: htmlContent },
-    });
-    const templateExt = template.split(".").reverse()[0];
-    await writeFile(`${outputFolder}/index.${templateExt}`, content);
-  });
+  await Promise.all(
+    posts.map(async (post) =>
+      convertPost({
+        post,
+        template,
+        templatePath,
+        outputDir,
+        folderFormat,
+        downloadImages,
+        filterImages,
+      })
+    )
+  );
 }
 
-run();
+module.exports = convert;
