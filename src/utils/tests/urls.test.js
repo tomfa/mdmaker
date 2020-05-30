c4d81cc19d060d54f74bb1d943414e9dd93c87f3
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-05-30 09:37:59 +0200

    utils: add utils to extract links from text

--- src/utils/tests/urls.test.js
+++ src/utils/tests/urls.test.js
@@ -1,25 +1,88 @@
-const { makeUrlsRelative } = require("../urls");
+const utils = require("../urls");
 
 describe("urls utils", () => {
   describe("makeUrlsRelative", () => {
     test("changes urls to relative", () => {
       const content = '<img src="http://google.com/image.png" />';
       const urls = ["http://google.com/image.png"];
-      expect(makeUrlsRelative({ content, urls })).toBe(
+      expect(utils.makeUrlsRelative({ content, urls })).toBe(
         '<img src="./image.png" />'
       );
     });
     test("allows added custom url prefix", () => {
       const content = '<img src="http://google.com/image.png" />';
       const urls = ["http://google.com/image.png"];
-      expect(makeUrlsRelative({ content, urls, pathPrefix: "./images/" })).toBe(
-        '<img src="./images/image.png" />'
-      );
+      expect(
+        utils.makeUrlsRelative({ content, urls, pathPrefix: "./images/" })
+      ).toBe('<img src="./images/image.png" />');
     });
     test("ignores other urls", () => {
       const content = '<img src="http://google.com/image.png" />';
       const urls = [];
-      expect(makeUrlsRelative({ content, urls })).toBe(content);
+      expect(utils.makeUrlsRelative({ content, urls })).toBe(content);
+    });
+  });
+
+  describe("extractUrls", () => {
+    const content = `
+      <a href="#kitten"><h1 id="kitten">Want a cat?</h1></a> 
+      <a href="https://placekitten.com/408/287/maxsize"> 
+        <img src="https://placekitten.com/408/287" /> 
+      </a>
+      <a href="http://google.com/flamingo-large.jpg"/> 
+        <img src="http://google.com/flamingo.jpg"/> 
+      </a>
+      <a href="../../img/digidog.png">
+        <img src="./doggy-small.jpg" />
+      </a>
+      <img src="./CATZILLA.PNG" />
+      <img src="logo.svg" />
+      "There you go."`;
+
+    test("returns img src", () => {
+      expect(utils.extractUrls({ content })).toContain(
+        "https://placekitten.com/408/287"
+      );
+      expect(utils.extractUrls({ content })).toContain(
+        "http://google.com/flamingo.jpg"
+      );
+    });
+    test("ignores #hashbang urls", () => {
+      expect(utils.extractUrls({ content })).not.toContain("#kitten");
+    });
+    test("includes href links with file ending", () => {
+      expect(utils.extractUrls({ content })).toContain(
+        "http://google.com/flamingo-large.jpg"
+      );
+    });
+    test("ignores href links without file ending", () => {
+      expect(utils.extractUrls({ content })).not.toContain(
+        "https://placekitten.com/408/287/maxsize"
+      );
+    });
+    test("ignores relative urls", () => {
+      expect(utils.extractUrls({ content })).not.toContain("./doggy-small.jpg");
+      expect(utils.extractUrls({ content })).not.toContain("./logo.svg");
+    });
+
+    test("filters urls if filterDomain is specified", () => {
+      expect(
+        utils.extractUrls({ content, filterDomain: "placekitten.com" })
+      ).toContain("https://placekitten.com/408/287");
+      expect(
+        utils.extractUrls({ content, filterDomain: "google.com" })
+      ).toContain("http://google.com/flamingo.jpg");
+    });
+
+    test("returns empty string if no matches found", () => {
+      const content = "<h1>HI</h1>";
+      expect(utils.extractUrls({ content })).toEqual([]);
+    });
+
+    test("can have regexp match overridden", () => {
+      const content = "<h1>header</h1>";
+      const regexp = "(?:>(.*?)<)";
+      expect(utils.extractUrls({ content, regexp })).toEqual(["header"]);
     });
   });
 });
--- src/utils/urls.js
+++ src/utils/urls.js
@@ -15,4 +15,37 @@ function makeUrlsRelative({ content, urls, pathPrefix = "./" }) {
   return cleanedContent;
 }
 
-module.exports = { makeUrlsRelative };
+const IMAGE_FILE_EXT = [
+  "apng",
+  "bmp",
+  "gif",
+  "cur",
+  "ico",
+  "jpg",
+  "jpeg",
+  "jfif",
+  "pjpeg",
+  "pjp",
+  "png",
+  "svg",
+  "webp",
+];
+
+let fileExtRegex = "(?:\\." + IMAGE_FILE_EXT.join("|") + ")";
+let hrefRegex = `(?:href="(http.*?${fileExtRegex}))"`;
+let imgRegex = '(?:src="(http.*?)")';
+let defaultRegex = `${imgRegex}|${hrefRegex}`;
+
+function extractUrls({ content, regexp = defaultRegex, filterDomain } = {}) {
+  const filePattern = new RegExp(regexp, "gi");
+  const matches = [];
+
+  let m;
+  while ((m = filePattern.exec(content)) !== null) {
+    matches.push(m.slice(1).find((u) => !!u));
+  }
+
+  return matches.filter((m) => (filterDomain ? m.includes(filterDomain) : m));
+}
+
+module.exports = { extractUrls, makeUrlsRelative };
