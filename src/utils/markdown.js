83872d1f21927c145a824cc2705a6698736e6eb2
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-05-30 16:56:34 +0200

    Replace package for converting html to md

--- src/utils/markdown.js
+++ src/utils/markdown.js
@@ -1,26 +1,15 @@
-const toMarkdown = require("to-markdown");
+const TurndownService = require("turndown");
+const turndownService = new TurndownService({ codeBlockStyle: "fenced" });
+
+turndownService.addRule("pre-is-code", {
+  filter: ["pre"],
+  replacement: function(content) {
+    return "\n\n```\n" + content + "\n```\n\n";
+  },
+});
 
 function htmlToMd(html) {
-  const markdown = toMarkdown(html);
-  return cleanContent(markdown);
-}
-
-function cleanContent(markdownContent) {
-  // Replaces characters that are misinterpreted by Markdown
-
-  return markdownContent
-    .replace(/[\u2018|\u2019|\u201A]/g, "'")
-    .replace(/&quot;/g, '"')
-    .replace(/[\u201C|\u201D|\u201E]/g, '"')
-    .replace(/\u2026/g, "...")
-    .replace(/[\u2013|\u2014]/g, "-")
-    .replace(/\u02C6/g, "^")
-    .replace(/\u2039/g, "<")
-    .replace(/&lt;/g, "<")
-    .replace(/\u203A/g, ">")
-    .replace(/&gt;/g, ">")
-    .replace(/[\u02DC|\u00A0]/g, " ")
-    .replace(/&amp;/g, "&");
+  return turndownService.turndown(html);
 }
 
 module.exports = { htmlToMd };
--- src/utils/tests/markdown.test.js
+++ src/utils/tests/markdown.test.js
@@ -1,34 +1,21 @@
 const { htmlToMd } = require("../markdown");
 
-const assert = (anything) => expect(anything).toBe(true);
-
 function assertMdPrefixes([htmlTag, mdPrefix]) {
   const textNode = "header";
   const htmlContent = `<${htmlTag}>${textNode}</${htmlTag}>`;
   const mdContent = mdPrefix ? `${mdPrefix} ${textNode}` : `${textNode}`;
-
-  assert(htmlToMd(htmlContent) === mdContent);
 }
 
 describe("markdown utils", () => {
-  test("converts headings", () => {
-    const headings = {
-      h1: "#",
-      h2: "##",
-      h3: "###",
-      h4: "####",
-      h5: "#####",
-      h6: "######",
-    };
-
-    Object.entries(headings).forEach(assertMdPrefixes);
+  test("converts h1", () => {
+    expect(htmlToMd("<h1>header</h1>")).toBe("header\n======");
   });
 
   test("converts images", () => {
     const htmlContent = `<img src="./fish.jpg" alt="A fish" />`;
     const mdContent = "![A fish](./fish.jpg)";
 
-    assert(htmlToMd(htmlContent) === mdContent);
+    expect(htmlToMd(htmlContent)).toBe(mdContent);
   });
 
   test("converts unordered lists", () => {
@@ -41,6 +28,24 @@ describe("markdown utils", () => {
   test("converts ordered lists", () => {
     const htmlContent = `<ol><li>a</li><li>b</li></ol>`;
     const mdContent = "1.  a\n2.  b";
+
+    expect(htmlToMd(htmlContent)).toBe(mdContent);
+  });
+
+  test("converts pre blocks", () => {
+    const htmlContent = `
+    <pre>This is some code</pre>
+    `;
+    const mdContent = "```\nThis is some code\n```";
+
+    expect(htmlToMd(htmlContent)).toBe(mdContent);
+  });
+
+  test("converts pre+code blocks", () => {
+    const htmlContent = `
+    <pre><code>This is some code</code></pre>
+    `;
+    const mdContent = "```\nThis is some code\n```";
 
     expect(htmlToMd(htmlContent)).toBe(mdContent);
   });
