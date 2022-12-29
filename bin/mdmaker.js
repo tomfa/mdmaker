36e2180194e250c075c0b4b55774f7d4e0e83aba
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2022-12-29 01:48:42 +0100

    add options to print available template args

--- bin/mdmaker.js
+++ bin/mdmaker.js
@@ -10,18 +10,21 @@ const defaultTemplate = "./templates/gatsby.md";
 const logger = require('../src/utils/logger')
 
 const argv = require("yargs")
-  .usage("yarn convert <input-file> [args]")
+  .usage("mdex <input-file> [args]")
   .example(
-    "yarn convert wordpress.xml",
+    "mdex wordpress.xml",
     "Generates markdown files based on wordpress xml export"
   )
   .example(
-    "yarn convert wordpress.xml -d",
+    "mdex wordpress.xml -d",
     "Downloads linked images (hosted on same domain)"
   )
+  .example('mdex export.xml --template=my-template.mdx', 'Export content to custom template. See also --template-args.')
+  .describe('template-args', 'Print args available in custom template')
+  .boolean('template-args')
   .alias("d", "download-images")
   .default("d", false)
-  .describe("d", "Downloads images refernces to poot folder.")
+  .describe("d", "Downloads images references to post folder.")
   .boolean("d")
   .describe("debug", "Log for debug purposes")
   .default("debug", false)
@@ -55,11 +58,22 @@ const parsePath = argv.parser !== defaultParser
   ? resolve(process.cwd(), argv.parser)
   : resolve(__dirname, `../src/${argv.parser}`);
 
-
 const template =
   argv.template !== defaultTemplate
     ? resolve(process.cwd(), argv.template)
     : resolve(__dirname, `../src/${argv.template}`);
+
+if (argv.templateArgs) {
+  logger.info('\nAvailable template args to use with custom template:')
+
+  logger.info('  {{ title }}    Title of post')
+  logger.info('  {{ date }}     Date post is published')
+  logger.info('  {{ image }}    Absolute URL of post main image')
+  logger.info('  {{ tags }}     Post tags, e.g')
+  logger.info('  {{ author }}   Author of post')
+  logger.info('  {{ status }}   e.g. publish')
+  logger.info('  {{ content }}  Content of post in .md format\n')
+}
 
 const { downloadImages, folderFormat, filterImages, outputDir, slug } = argv;
 const inputArg = (argv._.length && argv._[0]) || null;
